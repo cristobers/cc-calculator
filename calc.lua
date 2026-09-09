bef7ebc2bce74fe6da6f42b341478515ed78fe35
cristober <72763707+cristobers@users.noreply.github.com>
2026-09-09 15:09:03 +0100

    cleaning up
    i got bit by lua's strange examples of doing OOP which came back to bite me, also i've type hinted everything because i don't like the LSP not telling me when i'm giving a function the wrong arguments.

--- calc.lua
+++ calc.lua
@@ -1,4 +1,8 @@
 ---@diagnostic disable: lowercase-global
+---@diagnostic enable: inject-field
+---@diagnostic enable: need-check-nil
+---@diagnostic enable: undefined-field
+---@diagnostic enable: missing-fields
 
 --[[
 
@@ -42,10 +46,12 @@ TokenType = {
 ---@class Token
 ---@field type TokenType
 ---@field text string
+---@field value number | nil
 Token = {type = TokenType.EMPTY, text = "", value = nil}
 
 ---@param text string
 ---@param type TokenType
+---@param value number | nil
 function Token:new(object, type, text, value)
     local object = object or {}
     setmetatable(object, self)
@@ -78,11 +84,13 @@ function Scanner:new(source)
     return object
 end
 
+---@return boolean
 function Scanner:isAtEnd()
     return self.current > #self.source
 end
 
 -- Updates scanner's tokens table, which represents a list of tokens.
+---@return nil 
 function Scanner:scanTokens()
     while not self:isAtEnd() do
         if self.hadError then return end
@@ -113,6 +121,7 @@ function Scanner:scanTokens()
     self:addToken(Token:new(nil, TokenType.EOF, '\0'))
 end
 
+---@return nil 
 function Scanner:number()
     -- So long as we're a number, keep going forward.
     while self:isDigit(self:peek()) do
@@ -130,7 +139,7 @@ function Scanner:number()
                 .. self.source:sub(self.start, self.current - 1)
                 .. " no digits found after the decimal point."
             )
-            return
+            return nil
         end
 
         -- Keep consuming numbers to fill out the floating point.
@@ -149,12 +158,13 @@ function Scanner:number()
     ))   
 end
 
+---@return string
 function Scanner:peekNext()
     if (self.current + 1 >= #self.source) then return '\0' end
-    -- print(self.source, self.current, self.current + 1, #self.source)
     return self.source[self.current + 1]
 end
 
+---@return nil
 function Scanner:error(msg)
     self.hadError = true
     print("Error at characters: " .. self.start .. "/".. self.current-1)
@@ -162,15 +172,18 @@ function Scanner:error(msg)
 end
 
 ---@param char string
+---@return boolean
 function Scanner:isDigit(char)
     return char >= '0' and char <= '9'
 end
 
+---@return boolean
 function Scanner:isWhitespace(char)
     return char == ' '
 end
 
 -- Consumes the current token, and advances current.
+---@return string
 function Scanner:advance()
     local char = self.source:sub(self.current, self.current)
     self.current = self.current + 1
@@ -178,16 +191,19 @@ function Scanner:advance()
 end
 
 -- Returns the current token, doesn't consume it, doesn't advance current.
+---@return string
 function Scanner:peek()
     if self:isAtEnd() then return '\0' end
     return self.source:sub(self.current, self.current)
 end
 
 ---@param token Token
+---@return nil
 function Scanner:addToken(token)
     table.insert(self.tokens, token)
 end
 
+---@return nil
 function Scanner:debugPrint(tokens)
     print("DEBUG OUTPUT")
     ---@param value Token 
@@ -234,14 +250,16 @@ function Binary:new(left, operator, right)
 end
 
 ---@class Literal
----@field value any
+---@field value number
 Literal = {}
 
-function Literal:new(object, value)
-    local object = object or {}
+function Literal:new(value)
+    local object = {
+        value = value
+    }
     setmetatable(object, self)
     self.__index = self
-    object.value = value
+
     return object
 end
 
@@ -250,24 +268,26 @@ end
 ---@field expr Expr
 Unary = {}
 
-function Unary:new(object, token, expr)
-    local object = object or {}
+function Unary:new(token, expr)
+    local object = {
+        token = token,
+        expr = expr
+    }
     setmetatable(object, self)
     self.__index = self
-    self.token = token
-    self.expr = expr
+
     return object
 end
 
 ---@class Grouping
----@field value Expr
+---@field expr Expr
 Grouping = {}
-
-function Grouping:new(object, expr)
-    local object = object or {}
+function Grouping:new(expr)
+    local object = {
+        expression = expr
+    }
     setmetatable(object, self)
     self.__index = self
-    self.value = expr
     return object
 end
 
@@ -282,7 +302,8 @@ function Parser:new(tokens)
     local object = {
         ---@type Token[]
         tokens = tokens,
-        current = 1
+        current = 1,
+        hasError = false,
     }
     setmetatable(object, self)
     self.__index = self
@@ -313,7 +334,6 @@ function Parser:factor()
     while self:match(TokenType.DIVIDE, TokenType.MULTIPLY) do
         local operator = self:previous()
         local right = self:unary()
-        -- print(expr, operator, right)
         expr = Binary:new(expr, operator, right)
     end
     return expr
@@ -323,26 +343,30 @@ function Parser:unary()
     if self:match(TokenType.MINUS) then
         local operator = self:previous()
         local right = self:unary()
-        return Unary:new(nil, operator, right)
+        return Unary:new(operator, right)
     end
     return self:primary()
 end
 
 function Parser:primary()
     if self:match(TokenType.LITERAL) then
-        -- print(self:previous())
-        return Literal:new(nil, self:previous().value)
+        return Literal:new(self:previous().value)
     end
     if self:match(TokenType.OP_PAREN) then
         local expr = self:expression()
         self:consume(TokenType.CL_PAREN, "Expected ')' after expression.")
-        return Grouping:new(nil, expr)
+        if expr == nil then
+            self.hasError = true
+        end
+        if self.hasError then return nil end
+        return Grouping:new(expr)
     end
 end
 
 function Parser:consume(type, msg)
     if (self:check(type)) then return self:advance() end
-    print("we throw an error here......")
+    print("Error occured: " .. msg)
+    self.hasError = true
 end
 
 function Parser:match(...)
@@ -362,37 +386,43 @@ function Parser:check(type)
     return self:peek().type == type
 end
 
+---@return Token
 function Parser:advance()
     if not self:isAtEnd() then self.current = self.current + 1 end
     return self:previous()
 end
 
+---@return boolean
 function Parser:isAtEnd()
     return self:peek().type == TokenType.EOF
 end
 
+---@return Token
 function Parser:peek()
     return self.tokens[self.current]
 end
 
+---@return Token
 function Parser:previous()
     return self.tokens[self.current - 1]
 end
 
 -- ** Parser End ** --
 
+---@return string
 function paren(str)
     return "(" .. str .. ")"
 end
 
 -- ---@param expr Expr
+---@return string | nil
 function printExpression(expr)
     if getmetatable(expr) == Literal then
         return expr.value
     elseif getmetatable(expr) == Unary then
         return paren(expr.token.text .. " " .. printExpression(expr.expr))
     elseif getmetatable(expr) == Grouping then
-        return paren("group " .. printExpression(expr.value))
+        return paren("group " .. printExpression(expr.expression))
     elseif getmetatable(expr) == Token then
         return expr.text
     else
@@ -406,30 +436,34 @@ function printExpression(expr)
 end
 
 -- ---@param expr Expr
+---@return number | nil
 function evalExpression(expr)
     if getmetatable(expr) == Literal then
         return expr.value
     elseif getmetatable(expr) == Unary then
         return -evalExpression(expr.expr)
     elseif getmetatable(expr) == Grouping then
-        return evalExpression(expr.value)
+        return evalExpression(expr.expression)
     elseif getmetatable(expr) == Binary then
-        local left = evalExpression(expr.left)
-        local right = evalExpression(expr.right)
         if expr.operator.type == TokenType.MINUS then
-            return left - right
+            return evalExpression(expr.left) - evalExpression(expr.right)
+            -- return left - right
         elseif expr.operator.type == TokenType.PLUS then
-            return left + right
+            -- return left + right
+            return evalExpression(expr.left) + evalExpression(expr.right)
         elseif expr.operator.type == TokenType.DIVIDE then
-            return left / right
+            -- return left / right
+            return evalExpression(expr.left) / evalExpression(expr.right)
         elseif expr.operator.type == TokenType.MULTIPLY then
-            return left * right
+            -- return left * right
+            return evalExpression(expr.left) * evalExpression(expr.right)
         end
     end
     -- We've hit a case that's unknown.
     return nil
 end
 
+---@return nil
 function what_is_this_table(t)
     local output = "This is a "
     local result = getmetatable(t)
@@ -438,11 +472,13 @@ function what_is_this_table(t)
     elseif result == Unary then output = output .. "Unary" 
     elseif result == Grouping then output = output .. "Grouping"
     end
-    if result == nil then print("This is nil.") else 
-        print(tostring(t) .. " : " .. output) 
+    if result == nil then 
+        print("This is nil.") 
     end
+    print(tostring(t) .. " : " .. output)
 end
 
+---@return nil
 function main()
     print("CTRL+T to leave the REPL.")
     while true do
@@ -455,21 +491,58 @@ function main()
             goto start
         end
         local parser = Parser:new(scanner.tokens)
-        assert(parser.tokens == scanner.tokens, "wtf")
         local expression = parser:parse()
         if expression == nil then 
             print("expression was nil.") 
-            return
+            goto start
         end
         local result = evalExpression(expression)
+        -- printExpression(expression)
         if result == nil then print("we hit an error idk lol") goto start end
         print(result)
     end
 end
 
+---@return string
 function readInput()
     io.write("calc> ")
     return read()
 end
 
 main()
+
+---@return number | nil
+function testingFunction(input)
+    local scanner = Scanner:new(input)
+    scanner:scanTokens()
+    local parser = Parser:new(scanner.tokens)
+    local expression = parser:parse()
+    return evalExpression(expression)
+end
+
+---@return boolean
+function test(input, expectedValue)
+    local result = testingFunction(input)
+    if result ~= expectedValue then
+        print(
+        "Failed to pass test: " .. input .. ", was given value: " .. tostring(expectedValue)
+        .. ", expected: " .. result
+        )
+        return false 
+    end
+    return true
+end
+
+-- these are my sanity checks,
+-- if you see no output, then things are good.
+
+test("1", 1)
+test("(300 + (3 * 2))", 306)
+test("()", nil)
+test("(())", nil)
+test("(200 + 3) * 4", 812)
+test("200 + (3 * 4)", 212)
+test("6/2", 3)
+test("200 * 4", 800)
+test("5 + 5", 10)
+test("5 - 3", 2)
